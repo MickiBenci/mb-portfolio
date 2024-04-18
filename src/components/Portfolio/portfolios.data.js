4ec69dda5d4026587f28ace81add684183456cd6
Michelangelo Bencivenga <michelangelo.bencivenga@gmail.com>
2024-04-18 11:15:49 +0200

    Update Portfolios

--- src/components/Portfolio/portfolios.data.js
+++ src/components/Portfolio/portfolios.data.js
@@ -2,7 +2,7 @@ export const portfolios = [
   {
     name: "Homeprime",
     url: "https://homeprime.it",
-    logo: "https://homeprime.it/wp-content/themes/yootheme/cache/23/HomePrime-logo-23dc1754.png",
+    logo: "https://homeprime.it/wp-content/themes/yootheme/cache/70/HomePrime-logo-70d2c8cb.png",
     category: "Web App",
   },
   {
@@ -12,33 +12,45 @@ export const portfolios = [
     category: "Sito Vetrina",
   },
   {
+    name: "OuiFleurs",
+    url: "https://www.ouifleurs.com",
+    logo: "https://www.ouifleurs.com/wp-content/uploads/2024/02/logo-rosso-solo-scritta-menu.svg",
+    category: "Ecommerce",
+  },
+  {
+    name: "Larissa Barton",
+    url: "https://larissabarton.com",
+    logo: "https://larissabarton.com/wp-content/uploads/2024/04/logo-copertina-bk.png",
+    category: "Sito Vetrina",
+  },
+  {
     name: "GekoForGreen",
     url: "https://gekoforgreen.com/",
-    logo: "https://gekoforgreen.com/wp-content/themes/yootheme/cache/logo-GEKO-GREEN-e32e7605.webp",
+    logo: "https://gekoforgreen.com/wp-content/uploads/2021/05/logo-GEKO-GREEN.png",
     category: "Portale",
   },
   {
     name: "Franca Soavi",
     url: "https://francasoavi.com/",
-    logo: "https://francasoavi.com/wp-content/themes/yootheme/cache/82/logo-Franca-ok-82a9c873.png",
+    logo: "https://francasoavi.com/wp-content/uploads/2023/05/logo-Franca-ok.png",
     category: "Sito Vetrina",
   },
   {
     name: "Fable Farm",
     url: "https://fablefarm.it/",
-    logo: "https://fablefarm.it/wp-content/themes/yootheme/cache/17/fable-farm-logo-hi-q-17d41f92.png",
+    logo: "https://fablefarm.it/wp-content/uploads/2022/11/fable-farm-logo-hi-q.png",
     category: "Sito Vetrina",
   },
   {
     name: "Albero della Vita",
     url: "https://alberodellavitaparma.it/",
-    logo: "https://alberodellavitaparma.it/wp-content/themes/yootheme/cache/1c/Logo-Albero-della-Vita-38847a5e-240w-1c4460f1.png",
+    logo: "https://alberodellavitaparma.it/wp-content/uploads/2023/06/Logo-Albero-della-Vita-38847a5e-240w.png",
     category: "Ecommerce",
   },
   {
     name: "Scuola626",
     url: "https://scuola626.com/",
-    logo: "https://scuola626.com/wp-content/themes/yootheme/cache/be/logo-2017-ok-ok-ok-bed36f8c.png",
+    logo: "https://scuola626.com/wp-content/uploads/2023/02/logo-2017-ok-ok-ok.png",
     category: "Portale",
   },
   {
